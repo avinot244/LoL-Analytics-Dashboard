ebc158955ce51b93e32428bd586f7938ec4b10d1
avionot244 <aymericvinot38@gmail.com>
2024-02-25 20:15:41 +0100

    Adding links to our navbar

--- aeg-dashboard/src/components/App.js
+++ aeg-dashboard/src/components/App.js
@@ -1,14 +1,23 @@
 import '../styles/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import BasicExample from "./NavbarComp";
+import Home from "./Home"
+import LinkPage from "./LinkPage"
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+	{
+		path:'/',
+		element: <Home />
+	},
+	{
+		path:'/link',
+		element : <LinkPage />
+	}
+])
 
 function App() {
-  return (
-    <div className="App">
-      <BasicExample />
-      Hello
-    </div>
-  );
+	return <RouterProvider router={router}/>
 }
 
 export default App;
---
+++ aeg-dashboard/src/components/Home.js
@@ -0,0 +1,13 @@
+import BasicExample from "./NavbarComp"
+
+function Home(){
+    return(
+        <div>
+            <BasicExample />
+            <h1> This is the home page </h1>
+        </div>
+        
+    )
+}
+
+export default Home
