1a3e10155c1ebb18d903e34dc9d2d83e0d39a59a
avionot244 <aymericvinot38@gmail.com>
2024-03-06 22:39:49 +0100

    Fixing styling issue

--- aeg-dashboard/src/components/PlayerOverview.js
+++ aeg-dashboard/src/components/PlayerOverview.js
@@ -1,11 +1,62 @@
 import NavBarComp from "./NavbarComp"
 import "../styles/PlayerOverview.css"
+import SelectComp from "./SelectComp";
+import { useState } from "react";
+import ChampionIcon from "./ChampionIcon";
 
 function PlayerOverview(){
+    const patchList = [1.4, 1.14, 1.3];
+    const weekList = ["Week 1", "Week 2", "Week 3"]
+
+    const championList = ["Hwei", "Thresh", "Leona", "Maokai", "Senna", "Leona", "Nautilus"]
+
+    const [activePatch, setActivePatch] = useState('Select a patch')
+    const [activeWeek,  setActiveWeek] = useState('Select a week')
+
     return(
-        <div className="wrapper-overview">
+        
+        <div className="wrapper-overview-player">
             <NavBarComp />
             <h1> Player Overview </h1>
+            <div className="dashboard-playerOverview-controlPannel">
+                <ul className="dashboard-playerOverview-controlPannel-list">
+                    <li>
+                        <SelectComp 
+                            elementList={weekList}
+                            defaultValue={"-- Week --"}
+                            setActive={setActiveWeek}/>
+                    </li>
+                    <li>
+                        <SelectComp
+                            elementList={patchList}
+                            defaultValue={"-- Patch --"}
+                            setActive={setActivePatch}/>
+                    </li>
+                </ul>
+            </div>
+            <div className="playerOverview-content-wrapper">
+                <div className="playerOverview-graph">
+
+                </div>
+                <div className="playerOverview-other-content">
+                    <div className="playerOverview-stats">
+
+                    </div>
+                    <div className="playerOverview-champs">
+                        <h2>Best champs</h2>
+                        <ul className="playerOverview-champion-list">
+                            {championList.map((championName) => 
+                                <ChampionIcon
+                                    championName={championName}
+                                    winRate={50}
+                                    pickRate={60}
+                                />
+                            )}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            
         </div>
         
     )
