1930f98767103f5bcda51dd1f51e9a89e1a99e89
avionot244 <aymericvinot38@gmail.com>
2024-03-19 22:22:09 +0100

    Adding Draft component

---
+++ aeg-dashboard/src/components/ChampionIconSmall.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+import "../styles/ChampionIconSmall.css"
+
+function ChampionIconSmall({championName, width, height}) {
+    const apiURL = `https://ddragon.leagueoflegends.com/cdn/14.5.1/img/champion/${championName}.png`
+    const [img, setImg] = useState();
+
+    const fetchImage = async () => {
+        const res = await fetch(apiURL);
+        const imageBlob = await res.blob();
+        const imageObjectURL = URL.createObjectURL(imageBlob);
+        setImg(imageObjectURL);
+    };
+
+    useEffect(() => {
+        fetchImage();
+    }, []);
+
+    return (
+
+        <img className="champion-icon-small" src={img} alt={championName} width={width} height={height}/>
+
+    )
+}
+
+export default ChampionIconSmall;
