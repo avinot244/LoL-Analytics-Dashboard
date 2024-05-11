113899f0f7cfb09c00670b7bf8cf3657cc865951
avionot244 <aymericvinot38@gmail.com>
2024-05-11 16:15:19 +0200

    Fixed table

--- aeg-dashboard/src/components/Monitoring/PCAModelList.js
+++ aeg-dashboard/src/components/Monitoring/PCAModelList.js
@@ -1,67 +1,341 @@
-import { Button } from "@mui/material";
-import React from "react";
-import {Table} from "react-bootstrap"
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
-import DeleteIcon from '@mui/icons-material/Delete';
+
+import { useState } from "react";
+import PropTypes from 'prop-types';
+import { alpha } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import TableSortLabel from '@mui/material/TableSortLabel';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import SearchIcon from '@mui/icons-material/Search';
+import FilterListIcon from '@mui/icons-material/FilterList';
+import { visuallyHidden } from '@mui/utils';
+
+import "../../styles/PCAModelList.css"
+
+function descendingComparator(a, b, orderBy) {
+    if (b[orderBy] < a[orderBy]) {
+        return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+        return 1;
+    }
+    return 0;
+}
+
+function getComparator(order, orderBy) {
+    return order === 'desc'
+        ? (a, b) => descendingComparator(a, b, orderBy)
+        : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+// Since 2020 all major browsers ensure sort stability with Array.prototype.sort().
+// stableSort() brings sort stability to non-modern browsers (notably IE11). If you
+// only support modern browsers you can replace stableSort(exampleArray, exampleComparator)
+// with exampleArray.slice().sort(exampleComparator)
+function stableSort(array, comparator) {
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+        const order = comparator(a[0], b[0]);
+        if (order !== 0) {
+            return order;
+        }
+        return a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+}
+
+const headCells = [
+    {
+        id: 'uuid',
+        numeric: false,
+        disablePadding: true,
+        label: 'Model UUID'
+    },
+    {
+        id: 'role',
+        numeric: false,
+        disablePadding: true,
+        label: 'Role'
+    },
+    {
+        id: 'kmo',
+        numeric: true,
+        disablePadding: true,
+        label: 'KMO'
+    }
+]
+
+function EnhancedTableHead(props) {
+    const { onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } = props;
+    const createSortHandler = (property) => (event) => {
+        onRequestSort(event, property);
+    };
+
+    return (
+        <TableHead>
+            <TableRow>
+                <TableCell padding="checkbox">
+                    <Checkbox
+                        color="primary"
+                        indeterminate={numSelected > 0 && numSelected < rowCount}
+                        checked={rowCount > 0 && numSelected === rowCount}
+                        onChange={onSelectAllClick}
+                        inputProps={{
+                            'aria-label': 'select all Champions',
+                        }}
+                    />
+                </TableCell>
+
+                {headCells.map((headCell) => (
+                    <TableCell
+                        key={headCell.id}
+                        align={headCell.numeric ? 'right' : 'left'}
+                        padding={headCell.disablePadding ? 'none' : 'normal'}
+                        sortDirection={orderBy === headCell.id ? order : false}
+                        sx={{pr: "15px"}}
+                    >
+                        <TableSortLabel
+                            active={orderBy === headCell.id}
+                            direction={orderBy === headCell.id ? order : 'asc'}
+                            onClick={createSortHandler(headCell.id)}
+                        >
+                            {headCell.label}
+                            {orderBy === headCell.id ? (
+                                <Box component="span" sx={visuallyHidden}>
+                                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                                </Box>
+                            ) : null}
+                        </TableSortLabel>
+                    </TableCell>
+                ))}
+            </TableRow>
+        </TableHead>
+    );
+}
+
+EnhancedTableHead.propTypes = {
+    numSelected: PropTypes.number.isRequired,
+    onRequestSort: PropTypes.func.isRequired,
+    onSelectAllClick: PropTypes.func.isRequired,
+    order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+    orderBy: PropTypes.string.isRequired,
+    rowCount: PropTypes.number.isRequired,
+};
+
+function EnhancedTableToolbar(props) {
+    const { numSelected } = props;
+    return (
+        <Toolbar
+            sx={{
+                pl: { sm: 2 },
+                pr: { xs: 1, sm: 1 },
+                ...(numSelected > 0 && {
+                bgcolor: (theme) =>
+                    alpha(theme.palette.primary.main, theme.palette.action.activatedOpacity),
+                }),
+            }}
+        >
+        {numSelected > 0 ? (
+            <Typography
+                sx={{ flex: '1 1 100%' }}
+                color="inherit"
+                variant="subtitle1"
+                component="div"
+            >
+                
+            </Typography>
+            ) : (
+                <Typography
+                    sx={{ flex: '1 1 100%' }}
+                    variant="h6"
+                    id="tableTitle"
+                    component="div"
+                >
+                    PCA Model List
+                </Typography>
+            )}
+
+        {numSelected > 0 ? (
+            <Tooltip title="Search related drafts">
+                <IconButton
+                    onClick={() => alert("yo")}
+                >
+                    <SearchIcon />
+                </IconButton>
+            </Tooltip>
+        ) : (
+            <Tooltip title="Filter list">
+            <IconButton>
+                <FilterListIcon />
+            </IconButton>
+            </Tooltip>
+        )}
+        </Toolbar>
+    );
+}
+
+EnhancedTableToolbar.propTypes = {
+  numSelected: PropTypes.number.isRequired,
+};
+
 
 export default function PCAModelList ({modelList}) {
+    const [order, setOrder] = useState('asc');
+    const [orderBy, setOrderBy] = useState('championName');
+    const [selected, setSelected] = useState([]);
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(10);
+
+    const handleRequestSort = (event, property) => {
+        const isAsc = orderBy === property && order === 'asc';
+        setOrder(isAsc ? 'desc' : 'asc');
+        setOrderBy(property);
+    };
+
+    const handleSelectAllClick = (event) => {
+        if (event.target.checked) {
+            const newSelected = modelList.map((n) => n.pk);
+            setSelected(newSelected);
+            return;
+        }
+        setSelected([]);
+    };
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    const isSelected = (id) => selected.indexOf(id) !== -1;
+
+    // Avoid a layout jump when reaching the last page with empty rows.
+    const emptyRows =
+        page > 0 ? Math.max(0, (1 + page) * rowsPerPage - modelList.length) : 0;
+
+    
+    const visibleRows = stableSort(modelList, getComparator(order, orderBy)).slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+    // const visibleRows = React.useMemo(
+    //     () => stableSort(modelList, getComparator(order, orderBy)).slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage),
+    //     [order, orderBy, page, rowsPerPage],
+    // );
+
+    const handleClick = (event, id) => {
+        const selectedIndex = selected.indexOf(id);
+        let newSelected = [];
+
+        if (selectedIndex === -1) {
+            newSelected = newSelected.concat(selected, id);
+        } else if (selectedIndex === 0) {
+            newSelected = newSelected.concat(selected.slice(1));
+        } else if (selectedIndex === selected.length - 1) {
+            newSelected = newSelected.concat(selected.slice(0, -1));
+        } else if (selectedIndex > 0) {
+            newSelected = newSelected.concat(
+                selected.slice(0, selectedIndex),
+                selected.slice(selectedIndex + 1),
+            );
+        }
+        setSelected(newSelected);
+    };
+
     return (
-        <Table dark>
-            <thead>
-                <th>UUID</th>
-                <th>role</th>
-                <th>KMO</th>
-            </thead>
-            <tbody>
-                {!modelList || modelList.length <= 0 ? (
-                    <tr>
-                        <td colSpan={6} align="center">
-                            <b>Oops, no data yet</b>
-                        </td>
-                    </tr>
-                ) : (
-                    modelList.map(model => (
-                        <tr key={model.pk}>
-                            <td>{model.uuid}</td>
-                            <td>{model.role}</td>
-                            <td>{model.kmo}</td>
-                            <td align="center">
-                                <Button
-                                    endIcon={<ArrowForwardIosIcon/>}
-                                    variant="contained"
-                                >
-                                    Analyze
-                                </Button>
+        <div className='wrapper-tabpanel-pcaModelList'>
+            <Box>
+                <Paper sw={{width: '100%', mb: 2}}>
+                    <EnhancedTableToolbar numSelected={selected.length} />
+                    <TableContainer>
+                        <Table
+                            sx={{ minWidth: 750, pr: 5}}
+                            aria-labelledby="tableTitle"
+                            size={'small'}
+                        >
+                            <EnhancedTableHead
+                                numSelected={selected.length}
+                                order={order}
+                                orderBy={orderBy}
+                                onSelectAllClick={handleSelectAllClick}
+                                onRequestSort={handleRequestSort}
+                                rowCount={modelList.length}
+                            />
+                            <TableBody>
+                            {visibleRows.map((row, index) => {
+                                const isItemSelected = isSelected(row.pk);
+                                const labelId = `enhanced-table-checkbox-${index}`;
 
-                                <Button
-                                    endIcon={<ArrowDownwardIcon/>}
-                                    sx={{
-                                        ml: 2
+                                return (
+                                    <TableRow
+                                        hover
+                                        onClick={(event) => handleClick(event, row.pk)}
+                                        role="checkbox"
+                                        aria-checked={isItemSelected}
+                                        tabIndex={-1}
+                                        key={row.id}
+                                        selected={isItemSelected}
+                                        sx={{ cursor: 'pointer' }}
+                                    >
+                                        <TableCell padding="checkbox">
+                                            <Checkbox
+                                                color="primary"
+                                                checked={isItemSelected}
+                                                inputProps={{
+                                                    'aria-labelledby': labelId,
+                                                }}
+                                            />
+                                        </TableCell>
+                                        {/* <TableCell
+                                            component="th"
+                                            id={labelId}
+                                            scope="row"
+                                            padding="none"
+                                        >
+                                        </TableCell>
+                                         */}
+                                        <TableCell align="left">{row.uuid}</TableCell>
+                                        <TableCell align="left">{row.role}</TableCell>
+                                        <TableCell align="right">{row.kmo}</TableCell>
+                                        
+                                    </TableRow>
+                                );
+                            })}
+                            {emptyRows > 0 && (
+                                <TableRow
+                                    style={{
+                                        height: 33 * emptyRows,
                                     }}
-                                    variant="contained"
                                 >
-                                    Set
-                                </Button>
-
-                                <Button
-                                    endIcon={<DeleteIcon/>}
-                                    sx={{
-                                        ml: 2
-                                    }}
-                                    variant="contained"
-                                    color="error"
-                                >
-                                    Delete
-                                </Button>
-                            </td>
-                        </tr>
-                    ))
-                )
-
-                }
-            </tbody>
-        </Table>
-    )
+                                <TableCell colSpan={6} />
+                                </TableRow>
+                            )}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                    <TablePagination
+                        rowsPerPageOptions={[5, 10, 25]}
+                        component="div"
+                        count={modelList.length}
+                        rowsPerPage={rowsPerPage}
+                        page={page}
+                        onPageChange={handleChangePage}
+                        onRowsPerPageChange={handleChangeRowsPerPage}
+                    />
+                </Paper>
+            </Box>
+        </div>
+    );
 
 }
