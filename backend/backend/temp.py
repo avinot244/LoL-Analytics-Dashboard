e32e7e294db7f00d43cae1b7c5ab850e97878f0d
avinot244 <aymericvinot38@gmail.com>
2024-12-21 18:56:54 +0100

    Update .gitignore to include temp.py and refactor data processing in temp.py

--- backend/backend/temp.py
+++ backend/backend/temp.py
@@ -12,52 +12,10 @@ from dataAnalysis.packages.utils_stuff.globals import baseBlueBoundaries, baseRe
 
 (data, gameDuration, begGameTime, endGameTime) = getData(2729017, 5)
 print(gameDuration, begGameTime, endGameTime)
+splitList : list[int] = [120, 840]
+splittedData : list[SeparatedData] = data.splitData(gameDuration, splitList)
 
-# for snapshot in data.gameSnapshotList:
-#     current_time : float = snapshot.convertGameTimeToSeconds(gameDuration, begGameTime, endGameTime)
-gridBlueBase : Grid = Grid([Zone(baseBlueBoundaries)])
-gridRedBase : Grid = Grid([Zone(baseRedBoundaries)])
-result : dict = {
-    "blueTeam": {},
-    "redTeam": {}
-}
-
-firstSnapshot : Snapshot = data.gameSnapshotList[0]
-for player in firstSnapshot.teams[0].players:
-    result["blueTeam"][player.playerName] = []
-
-for player in firstSnapshot.teams[1].players:
-    result["redTeam"][player.playerName] = []
-
-
-
-def didPlayerReset(playerName : str, dataWindow : list[Snapshot], team : int):
-    participantID : int = dataWindow[0].teams[team].getPlayerID(playerName)
-    playerIdx : int = dataWindow[0].teams[team].getPlayerIdx(participantID)
-    positionFirstSecond : Position = dataWindow[0].teams[team].getPlayerPosition(playerIdx)
-    positionSecondSecond : Position = dataWindow[1].teams[team].getPlayerPosition(playerIdx)
+print(len(splittedData))
+for data in splittedData:
+    print(max([snap.convertGameTimeToSeconds(gameDuration, begGameTime, endGameTime) for snap in data.gameSnapshotList]))
     
-    if team == 0:
-        return not(gridBlueBase.containsPoint(positionFirstSecond)) and gridBlueBase.containsPoint(positionSecondSecond)
-    elif team == 1:
-        return not(gridRedBase.containsPoint(positionFirstSecond)) and gridRedBase.containsPoint(positionSecondSecond)
-
-for time in tqdm(range(120, gameDuration + 1)):
-    currentSnapshot : Snapshot = data.getSnapShotByTime(time, gameDuration)
-    dataWindow : list[Snapshot] = [data.getSnapShotByTime(t, gameDuration) for t in range(time, time+2, 1)]
-    
-    for player in currentSnapshot.teams[0].players:
-        if didPlayerReset(player.playerName, dataWindow, 0):
-            result["blueTeam"][player.playerName].append({
-                "time": time,
-                "position": player.position.__dict__
-            })
-            
-    for player in currentSnapshot.teams[1].players:
-        if didPlayerReset(player.playerName, dataWindow, 1):
-            result["redTeam"][player.playerName].append({
-                "time": time,
-                "position": player.position.__dict__
-            })
-
-print(json.dumps(result, indent=4))
