32aaba3df617d73ccf89f9204b926faf7ac5cce5
avinot244 <aymericvinot38@gmail.com>
2024-12-21 18:37:56 +0100

    Implement reset trigger logic for player position changes in reset_trigger.py

---
+++ backend/backend/dataAnalysis/packages/utils_stuff/reset_trigger.py
@@ -0,0 +1,20 @@
+from dataAnalysis.packages.Parsers.Separated.Game.Snapshot import Snapshot
+from dataAnalysis.packages.utils_stuff.Position import Position
+from dataAnalysis.packages.AreaMapping.Grid import Grid
+from dataAnalysis.packages.AreaMapping.Zone import Zone
+
+from dataAnalysis.packages.utils_stuff.globals import baseBlueBoundaries, baseRedBoundaries
+
+def didPlayerReset(playerName : str, dataWindow : list[Snapshot], team : int):
+    gridBlueBase : Grid = Grid([Zone(baseBlueBoundaries)])
+    gridRedBase : Grid = Grid([Zone(baseRedBoundaries)])
+    
+    participantID : int = dataWindow[0].teams[team].getPlayerID(playerName)
+    playerIdx : int = dataWindow[0].teams[team].getPlayerIdx(participantID)
+    positionFirstSecond : Position = dataWindow[0].teams[team].getPlayerPosition(playerIdx)
+    positionSecondSecond : Position = dataWindow[1].teams[team].getPlayerPosition(playerIdx)
+    
+    if team == 0:
+        return not(gridBlueBase.containsPoint(positionFirstSecond)) and gridBlueBase.containsPoint(positionSecondSecond)
+    elif team == 1:
+        return not(gridRedBase.containsPoint(positionFirstSecond)) and gridRedBase.containsPoint(positionSecondSecond)
