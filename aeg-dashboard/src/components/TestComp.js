3ba8d464631052c4ddf5bd04899b85b23ac3cd72
avinot244 <aymericvinot38@gmail.com>
2025-02-01 17:10:08 +0100

    CAsted fields value to float instead of the automatic string cast by JS

--- aeg-dashboard/src/components/TestComp.js
+++ aeg-dashboard/src/components/TestComp.js
@@ -1,110 +1,23 @@
 import NavBarComp from "./utils/NavbarComp"
 import "../styles/TestComp.css"
-import AuthContext from "./context/AuthContext"
-import ChampionIconSmall from "./utils/ChampionIconSmall"
-import GradientTypographyReverted from "./utils/GradientTypographyReverted"
 
-import { Typography, Button, Stack, Paper } from "@mui/material"
-import { DataGrid } from '@mui/x-data-grid';
-import { useDemoData } from '@mui/x-data-grid-generator';
+import { Typography } from "@mui/material"
 
 import { useState, useEffect, useContext } from "react"
 import { API_URL } from "../constants"
+import AuthContext from "./context/AuthContext"
+import DraftGrid from "./utils/DraftGrid"
 
 
-function RenderChampion(props){
-    const {_, value} = props
-    return (
-        <ChampionIconSmall championName={value} width={50} height={50}/>
-    )
-}
-
-function RenderDraftData(props) {
-    const {_, value} = props
-    return (
-        <GradientTypographyReverted value={value} bold>{value}%</GradientTypographyReverted>
-    )
-}
-
-function camelCaseToTitle(str) {
-    return str
-        .replace(/([A-Z])/g, ' $1') // Insert space before capital letters
-        .replace(/^./, match => match.toUpperCase()); // Capitalize the first letter
-}
 
 function TestComp() {
     const [rows, setRows] = useState([])
     // const [columns, setColumns] = useState([])
 
-    const columns = [
-        {
-            field: 'championName',
-            headerName: 'Champion',
-            width: 150,
-            renderCell: RenderChampion
-        },
-        {
-            field: 'winRate',
-            headerName: 'Win Rate',
-            width: 150,
-            renderCell: RenderDraftData
-        },
-        {
-            field: 'draftPresence',
-            headerName: 'Draft Presence',
-            width: 150,
-            renderCell: RenderDraftData
-        },
-        {
-            field: 'globalPickRate',
-            headerName: 'Global Pick Rate',
-            width: 150,
-            renderCell: RenderDraftData
-        },
-        {
-            field: 'pickRate1Rota',
-            headerName: 'Pick Rate 1st Rotation',
-            width: 150,
-            renderCell: RenderDraftData
-        },
-        {
-            field: 'pickRate2Rota',
-            headerName: 'Pick Rate 2nd Rotation',
-            width: 150,
-            renderCell: RenderDraftData
-        },
-        {
-            field: 'globalBanRate',
-            headerName: 'Global Ban Rate',
-            width: 150,
-            renderCell: RenderDraftData
-        },
-        {
-            field: 'banRate1Rota',
-            headerName: 'Ban Rate 1st Rotation',
-            width: 150,
-            renderCell: RenderDraftData
-        },
-        {
-            field: 'banRate2Rota',
-            headerName: 'Ban Rate 2nd Rotationi',
-            width: 150,
-            renderCell: RenderDraftData
-        },
-        {
-            field: 'blindPick',
-            headerName: 'Blind Pick',
-            width: 150,
-            renderCell: RenderDraftData
-        }
-    ]
-
     let {authTokens} = useContext(AuthContext)
     const header = {
         Authorization: "Bearer " + authTokens.access
     }
-
-    const paginationModel = { page: 0, pageSize: 5 };
 
     useEffect(() => {
         const fetchChampionsDraftStats = async (tournament, patch, side) => {
@@ -116,9 +29,9 @@ function TestComp() {
                 const newData = result;
                 let newRows = newData.map(({ pk, championName, patch, side, mostPopularRole, mostPopularPickOrder, tournament, ...rest }) => {
                     let updatedFields = Object.fromEntries(
-                        Object.entries(rest).map(([key, value]) => [key, (value * 100).toFixed(2)])
+                        Object.entries(rest).map(([key, value]) => [key, parseFloat((value * 100).toFixed(2))])
                     );
-                
+                    
                     return {
                         id: pk,
                         championName,
@@ -126,7 +39,7 @@ function TestComp() {
                     };
                 });
                 setRows(newRows)
-                console.log(newRows)
+                console.log(typeof newRows[0].winRate)
             })
         }
         
@@ -141,18 +54,8 @@ function TestComp() {
             <Typography id="title-Test" variant="h2" component="h1" align="center" sx={{mt: 10, fontWeight: "bold", mb: 10}}>
                 Test page
             </Typography>
-
-            <Paper sx={{ height: 370, width: '100%' }}>
-                <DataGrid
-                   rows={rows}
-                   columns={columns}
-                   initialState={{ pagination: { paginationModel } }}
-                   pageSizeOptions={[5, 10]}
-                   checkboxSelection
-                   sx={{ border: 0 }}
-                    
-                />
-            </Paper>
+            <DraftGrid rows={rows}/>
+            
         </div>
     )
 }
---
+++ aeg-dashboard/src/components/utils/DraftGrid.js
@@ -0,0 +1,104 @@
+import ChampionIconSmall from "./ChampionIconSmall"
+import GradientTypographyReverted from "./GradientTypographyReverted"
+
+import { Paper } from "@mui/material"
+import { DataGrid } from '@mui/x-data-grid';
+
+function RenderChampion(props){
+    const {_, value} = props
+    return (
+        <ChampionIconSmall championName={value} width={50} height={50}/>
+    )
+}
+
+function RenderDraftData(props) {
+    const {_, value} = props
+    return (
+        <GradientTypographyReverted value={value} bold>{value}%</GradientTypographyReverted>
+    )
+}
+
+function DraftGrid({ rows }) {
+    const columns = [
+        {
+            field: 'championName',
+            headerName: 'Champion',
+            width: 150,
+            renderCell: RenderChampion
+        },
+        {
+            field: 'winRate',
+            headerName: 'Win Rate',
+            width: 150,
+            renderCell: RenderDraftData
+        },
+        {
+            field: 'draftPresence',
+            headerName: 'Draft Presence',
+            width: 150,
+            renderCell: RenderDraftData
+        },
+        {
+            field: 'globalPickRate',
+            headerName: 'Global Pick Rate',
+            width: 150,
+            renderCell: RenderDraftData
+        },
+        {
+            field: 'pickRate1Rota',
+            headerName: 'Pick Rate 1st Rotation',
+            width: 160,
+            renderCell: RenderDraftData
+        },
+        {
+            field: 'pickRate2Rota',
+            headerName: 'Pick Rate 2nd Rotation',
+            width: 165,
+            renderCell: RenderDraftData
+        },
+        {
+            field: 'globalBanRate',
+            headerName: 'Global Ban Rate',
+            width: 150,
+            renderCell: RenderDraftData
+        },
+        {
+            field: 'banRate1Rota',
+            headerName: 'Ban Rate 1st Rotation',
+            width: 160,
+            renderCell: RenderDraftData
+        },
+        {
+            field: 'banRate2Rota',
+            headerName: 'Ban Rate 2nd Rotation',
+            width: 165,
+            renderCell: RenderDraftData
+        },
+        {
+            field: 'blindPick',
+            headerName: 'Blind Pick',
+            width: 150,
+            renderCell: RenderDraftData
+        }
+    ]
+
+    
+
+    const paginationModel = { page: 0, pageSize: 5 };
+
+    return (
+        <Paper sx={{ height: 370, width: 1600, justifySelf: "center"}}>
+            <DataGrid
+                rows={rows}
+                columns={columns}
+                initialState={{ pagination: { paginationModel } }}
+                pageSizeOptions={[5, 10]}
+                checkboxSelection
+                sx={{ border: 0 }}
+                
+            />
+        </Paper>
+    )
+}
+
+export default DraftGrid
